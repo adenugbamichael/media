a707c8d6b6eb67712062da0bd2470f30e44cff77
adenugbamichael <adenugbamicke@gmail.com>
2023-05-30 22:55:51 +0100

    Rendering the list | Changing data to mutations

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,10 +1,43 @@
 /* eslint-disable react/prop-types */
-import { useFetchAlbumsQuery } from "../store/apis/albumsApi"
+import {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+} from "../store/apis/albumsApi"
+import Skeleton from "./Skeleton"
+import ExpandablePanel from "./ExpandablePanel"
+import Button from "./Button"
 
 export default function AlbumList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user)
+  const [addAlbum, results] = useAddAlbumMutation()
 
-  console.log(data, error, isLoading)
+  const handleAddAlbum = () => {
+    addAlbum(user)
+  }
 
-  return <div>Albums for {user.name}</div>
+  let content
+  if (isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      )
+    })
+  }
+
+  return (
+    <div>
+      <div>
+        Albums for {user.name}
+        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  )
 }
