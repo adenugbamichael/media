28e87df672debeb38c665fb3a6d7a1ebbc518e32
adenugbamichael <adenugbamicke@gmail.com>
2023-05-30 20:10:19 +0100

    Create RTK query API with endpoint

--- src/components/ExpandablePanel.jsx
+++ src/components/ExpandablePanel.jsx
@@ -12,7 +12,10 @@ export default function ExpandablePanel({ header, children }) {
   return (
     <div className='mb-2 border rounded'>
       <div className='flex p-2 justify-between items-center '>
-        <div className='flex flex-row items-center justify-between'>
+        <div
+          onClick={handleClick}
+          className='flex flex-row items-center justify-between cursor-pointer'
+        >
           {header}
         </div>
         <div onClick={handleClick} className='cursor-pointer'>
